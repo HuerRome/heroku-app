144b6ca784fb546045a4103817786d1e9395775f
HuerRome <4n.rooo@gmail.com>
2022-02-14 02:54:29 -0300

    cambio

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+#/demo/views.py
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Hola Mundo!</h1>")
